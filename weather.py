fab739b877a8fa04622721e62a0f2fdae42d887e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-05-01 15:35:27 +0900

    delete

--- weather.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://api.open-meteo.com/v1/forecast?latitude=35.6918&longitude=139.762&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum,precipitation_sum,precipitation_hours,precipitation_probability_max&timezone=Asia%2FTokyo&forecast_days=1"
-
-response = requests.get(url)
-
-print(response.json())
